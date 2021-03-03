be48c6e4934ba013af5abbf19283a2382ba8b0e8
simon <simon.hengchen@gmail.com>
2021-03-03 09:23:59 +0100

    replacing dead bitbucket link for word2vecf with newer github one

--- scripts/get_packs.sh
+++ scripts/get_packs.sh
@@ -21,12 +21,9 @@ cd hyperwords
 chmod 755 *.sh
 chmod 755 scripts/*.sh
 
-mkdir word2vecf
-wget https://bitbucket.org/yoavgo/word2vecf/get/1b94252a58d4.zip
-unzip 1b94252a58d4.zip
-rm 1b94252a58d4.zip
-mv yoavgo-word2vecf-1b94252a58d4/*.c word2vecf/.
-mv yoavgo-word2vecf-1b94252a58d4/*.h word2vecf/.
-mv yoavgo-word2vecf-1b94252a58d4/makefile word2vecf/.
-rm -r yoavgo-word2vecf-1b94252a58d4
+
+wget https://github.com/BIU-NLP/word2vecf/archive/master.zip
+unzip master.zip
+rm master.zip
+mv word2vecf-master word2vecf
 make -C word2vecf word2vecf
